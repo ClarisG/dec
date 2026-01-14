e114e0ef630247ef258c8199932fc0df46ddf0cd
ClarisG <galvezclarisa85@gmail.com>
2026-01-14 12:52:01 +0800

    Update multiple files

--- register_personnel.php
+++ register_personnel.php
@@ -238,7 +238,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>LEIR - Personnel Registration</title>
+    <title>LEIR | Personnel Registration</title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
