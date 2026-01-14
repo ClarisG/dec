3db90f35a13708ce8d63dd3fc22410d0c6eeea48
ClarisG <galvezclarisa85@gmail.com>
2026-01-14 12:38:20 +0800

    Update multiple files

--- login.php
+++ login.php
@@ -535,6 +535,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     margin-top: 0;
     padding-top: 20px;
     border-radius: 30px 30px 0 0;
+    box-shadow: 0 -5px 20px rgba(0, 0, 0, 0.05);
     }
     
     .mobile-form-header {
@@ -567,7 +568,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         margin-top: 0;
         padding-top: 20px;
         border-radius: 30px 30px 0 0;
-        box-shadow: 0 -5px 20px rgba(0, 0, 0, 0.05);
     }
     
     .mobile-form-header {
